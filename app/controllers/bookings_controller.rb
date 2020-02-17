fd4a238f96448a5ed5868ccb2bd2cfb5b689770f
Margaux Roger <margaux831@live.fr>
2020-02-17 16:48:03 +0100

    controllers coded

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,30 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.caddie = Caddie.find(params[:id])
+    if @booking.save
+      redirect_to caddy_path(@booking.caddie)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,17 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.booking = params[:booking_id]
+    if @review.save
+      redirect_to caddy_path(@review.booking.caddie)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:)
+    params.require(:review).permit(:description,:rating)
   end
 
 end
