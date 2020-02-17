da29002cb44e4c8c63da0b5526dc5929c4daa5f6
Martin Faucheux <martin.faucheux@edhec.com>
2020-02-17 16:21:17 +0100

    models with validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :caddie
   belongs_to :user
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user, uniqueness: { scope: :caddie }
 end
--- app/models/caddie.rb
+++ app/models/caddie.rb
@@ -3,4 +3,9 @@ class Caddie < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   has_many :reviews, through: :bookings
+  validates :name, presence: true
+  validates :availability, presence: true
+  validates :condition, presence: true
+  validates :wheels_number, presence: true, numericality: { only_integer: true }
+  validates :capacity, presence: true
 end
