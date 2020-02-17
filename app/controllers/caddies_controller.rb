80e2dfcfb679f770dde7291ac14613e355f5c45e
Gaetan Brison <gaetan.brison@gmail.com>
2020-02-17 17:17:59 +0100

    v1 navbar

--- app/controllers/caddies_controller.rb
+++
@@ -1,2 +0,0 @@
-class CaddiesController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'caddies#home'
+  root to: 'pages#home'
   devise_for :users
 
   # resources :users, only: %i[show new create edit update delete]
