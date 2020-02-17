b600f26f399f6926a0b53ba9851118c1ee783286
Antoine Piguet <antoine.piguet@gmail.com>
2020-02-17 15:34:56 +0100

    Added routes and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'caddies#index'
+  devise_for :users
+
+  # resources :users, only: %i[show new create edit update delete]
+
+  resources :caddies do
+    resources :bookings, only: %i[new create] do
+    end
+  end
+
+  resources :bookings, only: :show do
+    resources :reviews, only: %i[new create]
+  end
+
+  get "/dashboard", to: "pages#dashboard"
+  resources :bookings, only: :delete
 end
