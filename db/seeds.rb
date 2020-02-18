e884dca904624a9f81692792df7527e1d9a688f7
Martin Faucheux <martin.faucheux@edhec.com>
2020-02-18 15:09:25 +0100

    caddies index grid div without map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mycaddie = Caddie.new(name: "Le Caddie de Mamie",
+                      condition: "Brand New",
+                      wheels_number: 4,
+                      capacity: "20L",
+                      user: User.find(1),
+                      availability: true)
+mycaddie.save!
+
+mycaddie = Caddie.new(name: "Le Caddie de Mamieee",
+                      condition: "Brand New",
+                      wheels_number: 4,
+                      capacity: "20L",
+                      user: User.find(1),
+                      availability: true)
+mycaddie.save!
+
+mycaddie = Caddie.new(name: "Leeeee Caddie de Mamie",
+                      condition: "Brand New",
+                      wheels_number: 4,
+                      capacity: "20L",
+                      user: User.find(1),
+                      availability: true)
+mycaddie.save!
