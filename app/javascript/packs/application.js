10d5bba104b938a9cdb160f57540c97c1531ecf9
Margaux Roger <margaux831@live.fr>
2020-02-19 10:54:15 +0100

    Add dotenv - Protect my secret data in .env file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,6 @@ import { loadDynamicBannerText } from 'components/banner';
 loadDynamicBannerText();
 import "../plugins/flatpickr";
 import "../bookings/new"
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
