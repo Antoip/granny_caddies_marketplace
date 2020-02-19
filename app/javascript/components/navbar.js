febaa2d31075456e384cc62632e382842dd310f1
Margaux Roger <margaux831@live.fr>
2020-02-19 12:28:40 +0100

    fixing navbar colors

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,27 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
+  const navLinks = document.querySelectorAll('.nav-link');
   if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-white');
-      } else {
-        navbar.classList.remove('navbar-lewagon-white');
-      }
-    });
+    const test_index = document.getElementById('home-card-list')
+    if (test_index) {
+      navbar.classList.add('fixed-top')
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= window.innerHeight) {
+          navbar.classList.add('navbar-lewagon-white');
+          navLinks.forEach((link) => {
+            link.classList.add('text-dark')
+          })
+        } else {
+          navbar.classList.remove('navbar-lewagon-white');
+          navLinks.forEach((link) => {
+            link.classList.remove('text-dark')
+          })
+        }
+      });
+    } else {
+      navbar.classList.add('sticky-top')
+      navbar.classList.add('navbar-lewagon-white')
+    }
   }
 }
 
