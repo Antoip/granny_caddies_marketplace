38835c3b906b31e1315dd8a06b9093609939bfa6
Margaux Roger <margaux831@live.fr>
2020-02-19 12:34:16 +0100

    last fix

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -5,16 +5,19 @@ const initUpdateNavbarOnScroll = () => {
     const test_index = document.getElementById('home-card-list')
     if (test_index) {
       navbar.classList.add('fixed-top')
+          navLinks.forEach((link) => {
+            link.classList.add('text-white')
+          })
       window.addEventListener('scroll', () => {
         if (window.scrollY >= window.innerHeight) {
           navbar.classList.add('navbar-lewagon-white');
           navLinks.forEach((link) => {
-            link.classList.add('text-dark')
+            link.classList.remove('text-white')
           })
         } else {
           navbar.classList.remove('navbar-lewagon-white');
           navLinks.forEach((link) => {
-            link.classList.remove('text-dark')
+            link.classList.add('text-white')
           })
         }
       });
